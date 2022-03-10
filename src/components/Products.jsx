19b2d4b292145e4224b3e37103f92769d9c3345a
zhika <zsultanova03@gmail.com>
2022-03-10 07:54:09 +0100

    add

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,7 @@
 import Product from "./Product"
 import { Grid } from "@mui/material"
 function Products({data}){
-    return(<><h1>Products, items</h1>
+    return(<><h1>Products 3</h1>
     <Grid container>
     {data.map((pr)=>{
         return(<Grid item xs={6}><Product data={pr} key={pr.id}/></Grid>)
